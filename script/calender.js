90df3a3b7db9d156f497caae6b37cae37db6199e
krasch <dev@krasch.io>
2024-09-11 12:49:51 +0200

    Basic drag&drop functionality for calender

--- script/calender.js
+++ script/calender.js
@@ -1,4 +1,4 @@
-function instantiateHourlyGridTemplate(hour){
+function instantiateHourlyGridTemplate(hour) {
     const template = document.getElementById('template-hourly-grid');
 
     // create new element by cloning template
@@ -10,7 +10,7 @@ function instantiateHourlyGridTemplate(hour){
     return element;
 }
 
-function instantiateConnectionGridTemplate(connection){
+function instantiateConnectionGridTemplate(id, connection) {
     const template = document.getElementById('template-connection-grid');
 
     // create new element by cloning template
@@ -18,25 +18,53 @@ function instantiateConnectionGridTemplate(connection){
 
     const resolution = 2;
     const rowStart = Math.round(connection.start * resolution)
-    const rowEnd =  Math.round(connection.end * resolution)
+    const rowEnd = Math.round(connection.end * resolution)
 
     // fill element attributes
+    element.id = id;
     element.innerText = connection.title;
     element.style.setProperty('grid-row-start', rowStart + 1);
     element.style.setProperty('grid-row-end', rowEnd + 1);
+    element.style.setProperty('grid-column', 1);
+
+    if (id === "1"){
+        element.style.setProperty('background-color', "blue");
+    }
+
+    element.addEventListener("dragstart", e => {
+        e.dataTransfer.dropEffect = "move";
+        e.dataTransfer.setData("connectionId", e.target.id);
+    });
+
+    element.addEventListener("drop", e => {
+        e.preventDefault();
+        const connectionId = e.dataTransfer.getData("connectionId");
+        const connectionDiv = document.getElementById(connectionId);
+
+        const connectionLength = connectionDiv.style.gridRowEnd - connectionDiv.style.gridRowStart;
+
+        connectionDiv.style.gridRowStart = e.target.style.gridRowStart;
+        connectionDiv.style.gridRowEnd = Number(e.target.style.gridRowStart) + connectionLength;
+        connectionDiv.style.zIndex = "2";
+
+    });
+
+    element.addEventListener("dragover", e => {
+        e.preventDefault();
+    });
 
     return element
 
 }
 
-function initHourlyGrid(container){
+function initHourlyGrid(container) {
     for (let hour = 0; hour < 24; hour++) {
         container.appendChild(instantiateHourlyGridTemplate(hour));
     }
 }
 
-function displayConnections(container, connections){
-    for (let i in connections){
-        container.appendChild(instantiateConnectionGridTemplate(connections[i]))
+function displayConnections(container, connections) {
+    for (let i in connections) {
+        container.appendChild(instantiateConnectionGridTemplate(i, connections[i]))
     }
 }
