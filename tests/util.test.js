593bcce3802dbf154e1993cfec837ffba5fbfe26
krasch <dev@krasch.io>
2024-09-13 15:41:22 +0200

    Try failing test

--- tests/util.test.js
+++ tests/util.test.js
@@ -3,5 +3,5 @@ let util = require("../script/util.js");
 test('consecutiveDaysShouldHaveOneDayDifference', function () {
     const date1 = new Date("2024-09-01");
     const date2 = new Date("2024-09-02");
-    expect(util.differenceInDays(date1, date2)).toBe(1);
+    expect(util.differenceInDays(date1, date2)).toBe(2);
 });
