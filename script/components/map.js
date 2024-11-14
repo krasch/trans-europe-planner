97528e7b06f33e8f9d6df41054b9b8df06440ba3
krasch <dev@krasch.io>
2024-11-14 14:43:55 +0100

    Leg and city manager

--- script/components/map.js
+++ script/components/map.js
@@ -57,6 +57,107 @@ class HoverState {
   }
 }
 
+class LegsManager {
+  #map;
+  #currentlyActive = [];
+
+  #callbacks = {
+    parentHover: () => {},
+    parentHoverEnd: () => {},
+  };
+
+  constructor(map) {
+    this.#map = map;
+
+    let hoverState = null;
+
+    // hover start
+    this.#map.on("mouseenter", "legs", (e) => {
+      hoverState = e.features.at(-1).id;
+      this.#callbacks["hover"](hoverState);
+    });
+
+    // hover done
+    this.#map.on("mouseleave", "legs", (e) => {
+      this.#callbacks["hoverEnd"](hoverState);
+      hoverState = null;
+    });
+  }
+
+  on(eventName, callback) {
+    this.#callbacks[eventName] = callback;
+  }
+
+  updateView(legs) {
+    // for simplicity, unset all previous state
+    for (let leg of this.#currentlyActive) {
+      const state = {
+        active: false,
+        color: null,
+        parent: null,
+        hover: false,
+      };
+      this.#map.setFeatureState({ source: "legs", id: leg.leg }, state);
+    }
+
+    // set new state
+    for (let leg of legs) {
+      const state = {
+        active: true,
+        color: `rgb(${leg.color})`,
+        parent: leg.parent,
+        hover: false,
+      };
+      this.#map.setFeatureState({ source: "legs", id: leg.leg }, state);
+    }
+
+    this.#currentlyActive = legs;
+  }
+}
+
+class CityManager {
+  #map;
+  #currentlyActive = [];
+
+  constructor(map) {
+    this.#map = map;
+  }
+
+  updateView(cities) {
+    // for simplicity, unset previously state
+    for (let city of this.#currentlyActive) {
+      const state = { color: null };
+      this.#map.setFeatureState({ source: "cities", id: city.city }, state);
+    }
+
+    // set new state
+    for (let city of cities) {
+      const state = { color: `rgb(${city.color})` };
+      this.#map.setFeatureState({ source: "cities", id: city.city }, state);
+    }
+    this.#currentlyActive = cities;
+
+    // can't filter by feature state which things are visible/not visible
+    // -> manually set filters by id
+    // all cities on the line get a circle
+    this.#map.setFilter(
+      "city-circle-stops-transfers",
+      this.#getIdFilter(cities.map((c) => c.city)),
+    );
+    // but only transfers also get a name
+    this.#map.setFilter(
+      "city-name-transfers",
+      this.#getIdFilter(cities.filter((c) => c.transfer).map((c) => c.city)),
+    );
+  }
+
+  #getIdFilter(items) {
+    const filter = ["in", "id"];
+    for (let i of items) filter.push(i);
+    return filter;
+  }
+}
+
 class MapWrapper {
   #callbacks = {
     legAdded: () => {},
@@ -65,7 +166,8 @@ class MapWrapper {
     legStopHover: () => {},
   };
 
-  #currentlyActiveLegs = [];
+  #legs = null;
+  #cities = null;
 
   constructor(containerId, center, zoom) {
     this.map = new maplibregl.Map({
@@ -109,6 +211,10 @@ class MapWrapper {
     // add all layers
     for (let layer of mapStyles) this.map.addLayer(layer);
 
+    // these two abstract away some of the details of dealing with the map items
+    this.#legs = new LegsManager(this.map);
+    this.#cities = new CityManager(this.map);
+
     // at mouseleave, map does not give us the id that the mouse left
     // so we need to keep track of it ourselves
     // the (e) => ... is necessary, otherwise we have the wrong "this"
@@ -145,60 +251,15 @@ class MapWrapper {
   }
 
   updateView(data) {
-    const [cities, activeLegs] = data;
+    const [cities, legs] = data;
 
-    // for simplicity, unset all previously active legs
-    for (let leg of this.#currentlyActiveLegs) {
-      this.#setFeatureState("legs", leg.leg, {
-        active: false,
-        color: null,
-        parent: null,
-        hover: false,
-      });
-    }
-
-    // draw new active legs
-    for (let leg of activeLegs) {
-      this.#setFeatureState("legs", leg.leg, {
-        active: true,
-        color: `rgb(${leg.color})`,
-        parent: leg.parent,
-        hover: false,
-      });
-    }
-    this.#currentlyActiveLegs = activeLegs;
-
-    for (let city of cities) {
-      this.#setFeatureState("cities", city.city, {
-        color: `rgb(${city.color})`,
-      });
-    }
-
-    const filter = ["in", "id"];
-    for (let city of cities) filter.push(city.city);
-    this.map.setFilter("city-circle-stops-transfers", filter);
-
-    const filter2 = ["in", "id"];
-    for (let city of cities.filter((c) => c.transfer)) filter2.push(city.city);
-    this.map.setFilter("city-name-transfers", filter2);
-
-    /*const [legs, color] = data; // todo fold into legs
-
-    const coloredLines = legs.filter((l) => l.active).map(legToGeojson);
-    this.#connections.update(asGeojsonFeatureCollection(coloredLines));
-    this.#connections.setPaintProperty("line-color", `rgb(${color})`);
-
-    const greyLines = legs.filter((l) => !l.active).map(legToGeojson);
-    this.#legs.update(asGeojsonFeatureCollection(greyLines));
-
-    const startCities = legs.filter((l) => l.active).map((c) => c.startCity);
-    const endCities = legs.filter((l) => l.active).map((c) => c.endCity);
-    const cities = new Set(startCities.concat(endCities)); // todo only works if city object
-    const markers = Array.from(cities).map(cityToGeojson);
-    this.#cities.update(asGeojsonFeatureCollection(markers));*/
+    // todo it might be visually more pleasing to first
+    // todo remove both old legs and cities and then draw the new legs and cities
+    this.#legs.updateView(legs);
+    this.#cities.updateView(cities);
   }
 
-  setHover(leg) {
+  /*setHover(leg) {
     for (let active of this.#currentlyActiveLegs) {
       if (leg !== active.parent) continue;
       this.#updateFeatureState("legs", active.leg, { hover: true });
@@ -210,21 +271,7 @@ class MapWrapper {
       if (leg !== active.parent) continue;
       this.#updateFeatureState("legs", active.leg, { hover: false });
     }
-  }
-
-  #getFeatureState(source, id) {
-    return this.map.getFeatureState({ source: source, id: id });
-  }
-
-  #setFeatureState(source, id, state) {
-    this.map.setFeatureState({ source: source, id: id }, state);
-  }
-
-  #updateFeatureState(source, id, newState) {
-    const state = this.#getFeatureState(source, id);
-    for (let key in newState) state[key] = newState[key];
-    this.#setFeatureState(source, id, state);
-  }
+  }*/
 }
 
 // exports for testing only (NODE_ENV='test' is automatically set by jest)
