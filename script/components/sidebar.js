e1469e0a15692aabd45781405e948524bb110502
krasch <dev@krasch.io>
2024-12-27 00:55:34 +0100

    Show and hide sidebar buttons

--- script/components/sidebar.js
+++ script/components/sidebar.js
@@ -1,82 +1,133 @@
 class Sidebar {
   #borderRadius;
 
-  #logoContainer;
-  #datePickerContainer;
-  #calendarContainer;
+  // relevant HTML elements
+  #logo;
+  #datePicker;
+  #calendar;
+  #showSidebarButton;
+  #hideSidebarButton;
+
+  // state
+  #datePickerShouldBeVisible = false;
+  #calendarShouldBeVisible = false;
+  #collapsed = false;
 
   constructor(container) {
     this.#borderRadius = window
       .getComputedStyle(container)
       .getPropertyValue("--border-radius");
 
-    this.#logoContainer = container.querySelector(".logo");
-    this.#datePickerContainer = container.querySelector(".header");
-    this.#calendarContainer = container.querySelector(".content");
-  }
+    this.#logo = container.querySelector(".logo");
+    this.#datePicker = container.querySelector(".header");
+    this.#calendar = container.querySelector(".content");
+    this.#showSidebarButton = container.querySelector("#show-sidebar");
+    this.#hideSidebarButton = container.querySelector("#hide-sidebar");
 
-  updateView(hasDate, hasActiveJourney) {
-    const hasBoth = hasDate && hasActiveJourney;
+    container.addEventListener("click", (e) => {
+      if (e.target.id === this.#hideSidebarButton.id) {
+        this.#collapsed = true;
 
-    if (hasActiveJourney && !this.#datePickerCurrentlyVisible) {
-      this.#showDatePicker();
-    } else if (!hasActiveJourney && this.#datePickerCurrentlyVisible) {
-      this.#hideDatePicker();
-    }
-
-    if (hasBoth && !this.#calendarCurrentlyVisible) {
-      this.#showCalendar();
-    } else if (!hasBoth && this.#calendarCurrentlyVisible) {
-      this.#hideCalendar();
-    }
-  }
-
-  get #datePickerCurrentlyVisible() {
-    return !this.#datePickerContainer.classList.contains("hidden");
-  }
-
-  get #calendarCurrentlyVisible() {
-    return !this.#calendarContainer.classList.contains("hidden");
-  }
-
-  #showDatePicker() {
-    this.#removeBorderRadius(this.#logoContainer);
-    this.#slideIn(this.#datePickerContainer);
-  }
-
-  #hideDatePicker() {
-    this.#slideOut(this.#datePickerContainer);
-    this.#logoContainer.style.borderBottomRightRadius = this.#borderRadius;
-  }
-
-  #showCalendar() {
-    this.#removeBorderRadius(this.#datePickerContainer);
-    this.#slideIn(this.#calendarContainer);
-  }
-
-  #hideCalendar() {
-    this.#slideOut(this.#calendarContainer);
-    this.#addBorderRadius(this.#datePickerContainer);
-  }
-
-  #slideIn(element) {
-    const width = window.getComputedStyle(element).getPropertyValue("width");
-
-    element.classList.remove("hidden");
-    element.animate([{ width: "0" }, { width: width }], {
-      duration: 200,
-      iterations: 1,
+        this.#updateView();
+      }
+      if (e.target.id === this.#showSidebarButton.id) {
+        this.#collapsed = false;
+        this.#updateView();
+      }
     });
   }
 
-  #slideOut(element) {
+  updateView(hasDate, hasActiveJourney) {
+    this.#datePickerShouldBeVisible = hasActiveJourney;
+    this.#calendarShouldBeVisible = hasActiveJourney && hasDate;
+    this.#updateView();
+  }
+
+  #updateView() {
+    if (this.#collapsed) {
+      this.#hideDatePicker();
+      this.#hideCalendar();
+      return;
+    }
+
+    if (this.#datePickerShouldBeVisible) this.#showDatePicker();
+    else this.#hideDatePicker();
+
+    if (this.#calendarShouldBeVisible) this.#showCalendar();
+    else this.#hideCalendar();
+  }
+
+  #showDatePicker() {
+    if (this.#isVisible(this.#datePicker)) return; // nothing to do
+
+    this.#removeBorderRadius(this.#logo);
+    this.#setVisible(this.#datePicker);
+    this.#setInvisible(this.#showSidebarButton);
+
+    this.#slideIn(this.#datePicker, () => {
+      this.#setVisible(this.#hideSidebarButton);
+    });
+  }
+
+  #hideDatePicker() {
+    if (!this.#isVisible(this.#datePicker)) return; // nothing to do
+
+    this.#setInvisible(this.#hideSidebarButton);
+
+    this.#slideOut(this.#datePicker, () => {
+      this.#addBorderRadius(this.#logo);
+      this.#setInvisible(this.#datePicker);
+      this.#setVisible(this.#showSidebarButton);
+    });
+  }
+
+  #showCalendar() {
+    if (this.#isVisible(this.#calendar)) return; // nothing to do
+
+    this.#removeBorderRadius(this.#datePicker);
+    this.#slideIn(this.#calendar);
+    this.#setVisible(this.#calendar);
+  }
+
+  #hideCalendar() {
+    if (!this.#isVisible(this.#calendar)) return; // nothing to do
+
+    this.#slideOut(this.#calendar, () => {
+      this.#addBorderRadius(this.#datePicker);
+      this.#setInvisible(this.#calendar);
+    });
+  }
+
+  #isVisible(element) {
+    return !element.classList.contains("hidden");
+  }
+
+  #setVisible(element) {
+    element.classList.remove("hidden");
+  }
+
+  #setInvisible(element) {
+    element.classList.add("hidden");
+  }
+
+  #slideIn(element, onFinish = () => {}) {
+    const width = window.getComputedStyle(element).getPropertyValue("width");
+
+    const animation = element.animate([{ width: "0" }, { width: width }], {
+      duration: 200,
+      iterations: 1,
+    });
+    animation.onfinish = onFinish;
+  }
+
+  #slideOut(element, onFinish = () => {}) {
     const width = window.getComputedStyle(element).getPropertyValue("width");
 
     const animation = element.animate([{ width: width }, { width: 0 }], {
       duration: 200,
       iterations: 1,
     });
-    animation.onfinish = () => element.classList.add("hidden");
+    animation.onfinish = onFinish;
   }
 
   #removeBorderRadius(element) {
