663877087125ca3a55b243184a1be8b84eb929bb
krasch <dev@krasch.io>
2025-04-15 10:57:58 +0200

    Pagination controls work on desktop

--- script/components/carousel.js
+++ script/components/carousel.js
@@ -3,10 +3,11 @@
 
 export class Carousel {
   #slidesContainer;
-  #dotsContainer;
+  #controlContainer;
 
   #slides;
   #dots;
+  #arrows;
 
   #current;
 
@@ -14,18 +15,32 @@ export class Carousel {
     this.#slidesContainer = container.querySelector("#slides");
     this.#slides = this.#slidesContainer.querySelectorAll("*");
 
-    this.#dotsContainer = container.querySelector("#pagination");
-    this.#dots = this.#dotsContainer.querySelectorAll("img");
+    this.#controlContainer = container.querySelector("#pagination");
+    this.#dots = this.#controlContainer.querySelectorAll(".dot");
+    this.#arrows = this.#controlContainer.querySelectorAll(".arrow");
 
+    // hop quickly to initial position todo smooth on
     this.#scrollTo(initialPosition);
 
+    // when user uses the scrollbar (or swipes), we want to update the dots and arrows
     this.#slidesContainer.addEventListener("scroll", () => this.#updateState());
-    this.#dotsContainer.addEventListener("click", (e) => {
+
+    // control bar buttons
+    this.#controlContainer.addEventListener("click", (e) => {
       const closest = e.target.closest("img");
       if (!closest) return;
 
-      const idx = Number(closest.dataset.slide) - 1;
-      this.#scrollTo(idx);
+      // clicked on a dot
+      if (closest.classList.contains("dot")) {
+        const slide = Number(closest.dataset.slide);
+        this.#scrollTo(slide - 1); // scroll-to uses 0-indexed array
+      }
+
+      // clicked on an arrow
+      if (closest.classList.contains("arrow")) {
+        if (closest.dataset.slide === "prev") this.#scrollTo(this.#current - 1);
+        else this.#scrollTo(this.#current + 1);
+      }
     });
   }
 
@@ -39,17 +54,27 @@ export class Carousel {
 
   #scrollTo(newIdx) {
     this.#slidesContainer.scrollTo(this.#slideWidth * newIdx, 0);
-    this.#updateState(); // scrollTo does not trigger "scroll" event listener, need to call update specifically
+    this.#updateState(); // scrollTo does not trigger "scroll" event listener, need to call update explicitly
   }
 
   #updateState() {
     const newIdx = Math.round(this.#scrollPosition / this.#slideWidth);
     if (this.#current === newIdx) return;
 
-    // update the dots
+    // hide current dot
     if (this.#current !== undefined)
       this.#dots[this.#current].classList.remove("active");
+
+    // show new dot
     this.#dots[newIdx].classList.add("active");
+
+    // if at first slide, hide the "previous" arrow
+    if (newIdx === 0) this.#arrows[0].classList.remove("clickable");
+    else this.#arrows[0].classList.add("clickable");
+
+    // if at last slide, hide the "next" arrow
+    if (newIdx === 2) this.#arrows[1].classList.remove("clickable");
+    else this.#arrows[1].classList.add("clickable");
 
     this.#current = newIdx;
   }
