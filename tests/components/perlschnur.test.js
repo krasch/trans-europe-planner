49d8dd9f39bed8232ab5b903fb5342595f36cf70
krasch <dev@krasch.io>
2026-01-20 21:03:23 +0100

    Add tests for perlschnur events

--- tests/components/perlschnur.test.js
+++ tests/components/perlschnur.test.js
@@ -1,11 +1,15 @@
 /**
  * @vitest-environment jsdom
  */
-import { beforeEach, test, expect } from "vitest";
+import { beforeEach, test, expect, vi } from "vitest";
 
 import { Perlschnur } from "script/components/perlschnur.js";
 
-import { initTestDOM, timeout } from "tests/_helpers/domUtils.js";
+import {
+  dispatchTestEvent,
+  initTestDOM,
+  timeout,
+} from "tests/_helpers/domUtils.js";
 
 beforeEach(async () => {
   initTestDOM();
@@ -37,7 +41,7 @@ function _connection(id, icon, number, travelTime, color) {
 const domElements = {
   perlschnur: () => document.querySelector("#perlschnur"),
   connections: () => document.querySelectorAll(".perlschnur-connection"),
-  stops: (conn) => conn.querySelector(".perlschnur-stop-list").children,
+  stopList: (conn) => conn.querySelector(".perlschnur-stop-list"),
   transfers: () => document.querySelectorAll(".perlschnur-transfer"),
 };
 
@@ -95,7 +99,8 @@ test("update view should fill in all templates correctly", async function () {
   ]);
 
   // stops for connection 1
-  expect(domElements.stops(connections[0])).toMatchDOMObjectList([
+  let stops = domElements.stopList(connections[0]).children;
+  expect(stops).toMatchDOMObjectList([
     _stop("(15 Oct)", "10:01", "S1", "Stop1"),
     { selectors: { ".count": { innerText: 1 } } }, // collapse
     _stop("", "11:00", "S2", "Stop2"),
@@ -103,13 +108,15 @@ test("update view should fill in all templates correctly", async function () {
   ]);
 
   // stops for connection 2
-  expect(domElements.stops(connections[1])).toMatchDOMObjectList([
+  stops = domElements.stopList(connections[1]).children;
+  expect(stops).toMatchDOMObjectList([
     _stop("", "13:01", "S3", "Stop3"),
     _stop("", "14:00", "S4", "Stop4"),
   ]);
 
   // stops for connection 3
-  expect(domElements.stops(connections[2])).toMatchDOMObjectList([
+  stops = domElements.stopList(connections[2]).children;
+  expect(stops).toMatchDOMObjectList([
     _stop("", "17:01", "S4", "Stop4"),
     _stop("(16 Oct)", "07:00", "S5", "Stop5"),
   ]);
@@ -171,11 +178,116 @@ test("update view should update with new connection data", async function () {
   ]);
 
   // stops
-  expect(domElements.stops(connections[0])).toMatchDOMObjectList([
+  const stops = domElements.stopList(connections[0]).children;
+  expect(stops).toMatchDOMObjectList([
     _stop("(15 Oct)", "13:01", "S3", "Stop3"),
     _stop("", "14:00", "S4", "Stop4"),
   ]);
 
   // transfers
   expect(domElements.transfers().length).toBe(0);
+});
+
+test("hovering information should get transported into and out of element", async function () {
+  const data = [
+    {
+      id: "c1",
+      color: "red",
+      name: "ICE T1",
+      icon: "REGIONAL_RAIL.svg",
+      travelTime: "1h 59min",
+      stops: [
+        { stopId: "S1", stopName: "Stop1", time: "10:01", date: "(15 Oct)" },
+        { stopId: "S2", stopName: "Stop2", time: "11:00", date: "" },
+        { stopId: "S3", stopName: "Stop3", time: "12:00", date: "" },
+      ],
+      transferTime: null,
+    },
+  ];
+
+  const perlschnur = new Perlschnur(domElements.perlschnur());
+  perlschnur.updateView(data);
+  await timeout(10);
+
+  const connection = domElements.connections()[0];
+  const stops = domElements.stopList(connection).children;
+
+  const connectionHoverCallback = vi.fn();
+  const stopHoverCallback = vi.fn();
+  perlschnur.on("connectionHover", connectionHoverCallback);
+  perlschnur.on("stopHover", stopHoverCallback);
+
+  // hovering over stop (inside connection) triggers stop + connection callbacks
+  await dispatchTestEvent(stops[2], "mouseover");
+  expect(stopHoverCallback).toHaveBeenCalledWith("S2", true);
+  expect(connectionHoverCallback).toHaveBeenCalledWith("c1", true);
+  vi.resetAllMocks();
+
+  // same for no longer hovering over a stop
+  await dispatchTestEvent(stops[2], "mouseout");
+  expect(stopHoverCallback).toHaveBeenCalledWith("S2", false);
+  expect(connectionHoverCallback).toHaveBeenCalledWith("c1", false);
+  vi.resetAllMocks();
+
+  // hover over a connection and not a stop triggers only connection callback
+  await dispatchTestEvent(connection, "mouseover");
+  expect(connectionHoverCallback).toHaveBeenCalledWith("c1", true);
+  vi.resetAllMocks();
+
+  // same for no longer hovering over a connection
+  await dispatchTestEvent(connection, "mouseout");
+  expect(connectionHoverCallback).toHaveBeenCalledWith("c1", false);
+  vi.resetAllMocks();
+
+  // from outside: setting unknown stop/connection to hover is ignored
+  perlschnur.setStopHover("S4", true);
+  perlschnur.setConnectionHover("c4", true);
+
+  // from outside: set stop to hover/unhover
+  perlschnur.setStopHover("S1", true);
+  expect(Array.from(stops[0].classList)).contains("hover");
+  perlschnur.setStopHover("S1", false);
+  expect(Array.from(stops[0].classList)).not.contains("hover");
+
+  // from outside: set connection to hover/unhover
+  perlschnur.setConnectionHover("c1", true);
+  expect(Array.from(connection.classList)).contains("hover");
+  perlschnur.setConnectionHover("c1", false);
+  expect(Array.from(connection.classList)).not.contains("hover");
+});
+
+test("clicking collapse should switch collapse status", async function () {
+  const data = [
+    {
+      id: "c1",
+      color: "red",
+      name: "ICE T1",
+      icon: "REGIONAL_RAIL.svg",
+      travelTime: "1h 59min",
+      stops: [
+        { stopId: "S1", stopName: "Stop1", time: "10:01", date: "(15 Oct)" },
+        { stopId: "S2", stopName: "Stop2", time: "11:00", date: "" },
+        { stopId: "S3", stopName: "Stop3", time: "12:00", date: "" },
+      ],
+      transferTime: null,
+    },
+  ];
+
+  const perlschnur = new Perlschnur(domElements.perlschnur());
+  perlschnur.updateView(data);
+  await timeout(10);
+
+  const connection = domElements.connections()[0];
+  const stopList = domElements.stopList(connection);
+
+  // initial state
+  expect(stopList.dataset.collapsed).toBe("collapsed");
+
+  // expand
+  dispatchTestEvent(stopList, "click");
+  expect(stopList.dataset.collapsed).toBe("");
+
+  // collapsed
+  dispatchTestEvent(stopList, "click");
+  expect(stopList.dataset.collapsed).toBe("collapsed");
 });
