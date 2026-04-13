213ad04365d2f070e4d5433deaec80cf2506a4d6
krasch <dev@krasch.io>
2026-04-13 17:41:39 +0200

    Pull out component rendering from main render loop

--- app/render.js
+++ app/render.js
@@ -1,14 +1,67 @@
 import { prepareDataForCalendar } from "app/components/_data/calendar.js";
 import { prepareDataForMap } from "app/components/_data/map.js";
 import { prepareDataForPerlschnur } from "app/components/_data/perlschnur.js";
+import { CalendarWrapper } from "app/components/calendar.js";
+import { Config } from "app/components/config.js";
+import { MapWrapper } from "app/components/map.js";
+import { Perlschnur } from "app/components/perlschnur.js";
 // todo should all come from planner
 import { getStopInfo } from "app/data/motis/client.js";
 import { Planner } from "app/data/planner/planner.js";
+import { Connection } from "app/types/connection.js";
+import { DateTime } from "app/types/dateTime.js";
+import { Itinerary } from "app/types/itinerary.js";
+import { Stop } from "app/types/stop.js";
 import { setURLState } from "app/url.js";
 
 /**
  * @typedef {import("app/url.js").ParsedURLData} ParsedURLData
  */
+
+/**
+ * @param {Config} config
+ * @param {Stop} [from]
+ * @param {Stop} [to]
+ * @param {DateTime} [date]
+ */
+function renderConfig(config, from, to, date) {
+  config.updateView(from?.name, to?.name, date);
+}
+
+/**
+ * @param {Perlschnur} perlschnur
+ * @param {Itinerary} active
+ */
+function renderPerlschnur(perlschnur, active) {
+  const data = prepareDataForPerlschnur(active);
+  perlschnur.updateView(data);
+}
+
+/**
+ * @param {MapWrapper} map
+ * @param {Itinerary} active
+ * @param {Itinerary[]} alternatives
+ */
+async function renderMap(map, active, alternatives) {
+  const data = prepareDataForMap(active, alternatives);
+  await map.updateView(data);
+}
+
+/**
+ * @param {CalendarWrapper} calendar
+ * @param {Itinerary} active
+ * @param {DateTime} startDate
+ */
+function initRenderCalendar(calendar, active, startDate) {
+  /**
+   * @param {Connection[][]} alternatives
+   */
+  function render(alternatives) {
+    const calendarData = prepareDataForCalendar(active, alternatives);
+    calendar.updateView(startDate.toISODate(), calendarData);
+  }
+  return render;
+}
 
 /**
  * @param {Object.<string,any>} components
@@ -23,11 +76,7 @@ export async function render(components, planner, urlState) {
   if (urlState.to) to = await getStopInfo(urlState.to);
 
   // update config form
-  components.config.updateView(
-    from?.name,
-    to?.name,
-    urlState.calendarStartDate,
-  );
+  renderConfig(components.config, from, to, urlState.calendarStartDate);
 
   // form is not fully filled out -> nothing else to render
   if (!from || !to || !urlState.calendarStartDate) return;
@@ -66,45 +115,44 @@ export async function render(components, planner, urlState) {
 
   // we also need to gather all the data for the alternative itineraries
   // but since only the map needs it, we don't await
-  const alternativesPromises = urlState.alternatives.map((alt) =>
+  const mapAlternativesPromises = urlState.alternatives.map((alt) =>
     planner.itineraryForIds(alt, urlState.calendarStartDate),
   );
 
   // as soon as all the alternative itineraries are ready, we can draw the map
-  Promise.all(alternativesPromises).then((alternatives) => {
-    const mapData = prepareDataForMap(active, alternatives);
-    components.map.updateView(mapData);
-
-    // we can now also turn of the loading circle
-    // (the calendar events have their own loading circle so don't need the big one while loading them)
+  // we can then also turn of the main loading circle
+  // (the calendar events have their own loading circle so don't need the big one while loading them)
+  Promise.all(mapAlternativesPromises).then(async (mapAlternatives) => {
+    await renderMap(components.map, active, mapAlternatives);
     components.config.unlock();
   });
 
-  // draw the perlschnur
-  const perlschnurData = prepareDataForPerlschnur(active);
-  components.perlschnur.updateView(perlschnurData);
+  // perlschnur can be rendered without extra data
+  renderPerlschnur(components.perlschnur, active);
 
   // for calendar, we prepare a list [null, null, ...] with length = num connections in active itinerary
   // null = no alternatives available yet for this connection = show this connection in calendar with spinny wheel
-  const activeAlternatives = active.connections.map(() => null);
-  const renderCalendar = () => {
-    const calendarData = prepareDataForCalendar(active, activeAlternatives);
-    components.calendar.updateView(
-      urlState.calendarStartDate.toISODate(),
-      calendarData,
-    );
-  };
+  const calendarAlternatives = active.connections.map(() => null);
 
-  // we start by rendering the calendar with all connections having a spinny wheel
-  // then we load all the alternative and as soon as any are ready, we redraw the calendar, now with fewer spinny wheels
-  renderCalendar();
+  // we initialise calendar rendering with the parameters that are always the same
+  // (otherwise the lines get so long)
+  const renderCalendar = initRenderCalendar(
+    components.calendar,
+    active,
+    urlState.calendarStartDate,
+  );
+
+  // we start by rendering the calendar with all alternatives = null -> all connections having a spinny wheel
+  renderCalendar(calendarAlternatives);
   components.navigation.focusComponent("calendar");
+
+  // then we load all the alternative and as soon as any are ready, we redraw the calendar, now with fewer spinny wheels
   active.connections.forEach((connection, i) => {
     planner
       .alternativeConnections(connection, urlState.calendarStartDate)
       .then((alternativesForConnection) => {
-        activeAlternatives[i] = alternativesForConnection;
-        renderCalendar();
+        calendarAlternatives[i] = alternativesForConnection;
+        renderCalendar(calendarAlternatives);
       });
   });
 
