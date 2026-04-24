b1fb2666c6808c810c4a5a1a2f744e027317fb0d
krasch <dev@krasch.io>
2026-04-24 21:41:19 +0200

    Change default port

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start",
-    url: "http://localhost:8000",
+    url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
 });
