099e5259d690d9ec7b765d5101e0d2342ea78d17
krasch <dev@krasch.io>
2026-05-25 21:03:30 +0200

    Make test for readible

--- tests_e2e/tests.spec.ts
+++ tests_e2e/tests.spec.ts
@@ -1,32 +1,17 @@
 import { test, expect } from "./fixture.js";
 
-const C1 = "27, 158, 119";
-const C2 = "217, 95, 2";
+const COLORS = ["27, 158, 119", "217, 95, 2"];
 
 const STOPS = {
   stralsund: "de-DELFI_de:13073:10401_G",
   schwerin: "de-DELFI_de:13004:9005",
 };
 
-const NAMES = {
-  stralsund: "Stralsund Hauptbahnhof",
-  rostock: "Rostock Hauptbahnhof",
-  schwerin: "Schwerin Hauptbahnhof",
-};
-
 function timeout(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-test("One long interaction", async ({ page }) => {
-  const url = new URL("http://localhost:3000");
-  url.searchParams.set("from", STOPS.stralsund);
-  url.searchParams.set("to", STOPS.schwerin);
-  url.searchParams.set("calStart", "2026-05-24");
-
-  await page.goto(url.toString());
-  await timeout(5000); // enough time to render (important for screenshot)
-
+async function expectPage(page, exp) {
   const loc = {
     from: page.locator("#config-from"),
     to: page.locator("#config-to"),
@@ -47,69 +32,92 @@ test("One long interaction", async ({ page }) => {
     },
   };
 
-  await expect(loc.from).toHaveValue(NAMES.stralsund);
-  await expect(loc.to).toHaveValue(NAMES.schwerin);
-  await expect(loc.date).toHaveValue("2026-05-24");
+  const stops = [exp.from].concat(exp.via).concat([exp.to]);
 
-  await expect(loc.cal.all).toHaveCount(84);
-  await expect(loc.cal.active).toHaveCount(2);
-  await expect(loc.cal.inactive).toHaveCount(82);
+  // config form
+  await expect(loc.from).toHaveValue(exp.from);
+  await expect(loc.to).toHaveValue(exp.to);
+  await expect(loc.date).toHaveValue(exp.calStartDate);
+
+  // calendar
+  // #entry-parts might be different than #connections (multi-day connections)
+  await expect(loc.cal.all).toHaveCount(exp.calActive + exp.calInactive);
+  await expect(loc.cal.active).toHaveCount(exp.calActive);
+  await expect(loc.cal.inactive).toHaveCount(exp.calInactive);
+
   await expect(loc.cal.active).allToBeVisible();
   await expect(loc.cal.inactive).allToBeHidden();
-  await expect(loc.cal.active.nth(0)).toHaveCSS("--color", C1);
-  await expect(loc.cal.active.nth(1)).toHaveCSS("--color", C2);
-  await expect(loc.cal.activeStart.nth(0)).toContainText(NAMES.stralsund);
-  await expect(loc.cal.activeDest.nth(0)).toContainText(NAMES.rostock);
-  await expect(loc.cal.activeStart.nth(1)).toContainText(NAMES.rostock);
-  await expect(loc.cal.activeDest.nth(1)).toContainText(NAMES.schwerin);
 
-  await expect(loc.perl.connections).toHaveCount(2);
-  await expect(loc.perl.transfers).toHaveCount(1);
-  await expect(loc.perl.connections.nth(0)).toHaveCSS("--color", C1);
-  await expect(loc.perl.connections.nth(1)).toHaveCSS("--color", C2);
-  await expect(loc.perl.connections.nth(0)).toContainText(NAMES.stralsund);
-  await expect(loc.perl.connections.nth(0)).toContainText(NAMES.rostock);
-  await expect(loc.perl.connections.nth(1)).toContainText(NAMES.rostock);
-  await expect(loc.perl.connections.nth(1)).toContainText(NAMES.schwerin);
+  for (let i = 0; i < exp.active; i++) {
+    await expect(loc.cal.active.nth(i)).toHaveCSS("--color", COLORS[i]);
+  }
 
-  await expect(page).toHaveScreenshot("map1.png", {
+  for (let i = 0; i < exp.active; i++) {
+    await expect(loc.cal.activeStart.nth(i)).toContainText(stops[i]);
+    await expect(loc.cal.activeDest.nth(i)).toContainText(stops[i + 1]);
+  }
+
+  // perlschnur
+  await expect(loc.perl.connections).toHaveCount(exp.via.length + 1);
+  await expect(loc.perl.transfers).toHaveCount(exp.via.length);
+
+  for (let i = 0; i < exp.active; i++) {
+    await expect(loc.perl.connections.nth(i)).toHaveCSS("--color", COLORS[i]);
+  }
+
+  for (let i = 0; i < exp.active; i++) {
+    await expect(loc.perl.connections.nth(i)).toContainText(stops[i]);
+    await expect(loc.perl.connections.nth(i + 1)).toContainText(stops[i + 1]);
+  }
+
+  // map
+  await timeout(1000);
+  await expect(page).toHaveScreenshot(exp.mapScreenshot, {
     stylePath: "tests_e2e/mapScreenshot.css",
   });
+}
+
+test("One long interaction", async ({ page }) => {
+  const expStralsundToSchwerinViaRostock = {
+    from: "Stralsund Hauptbahnhof",
+    to: "Schwerin Hauptbahnhof",
+    via: ["Rostock Hauptbahnhof"],
+    calStartDate: "2026-05-24",
+    calActive: 2,
+    calInactive: 82,
+    mapScreenshot: "map1.png",
+  };
+
+  const expStralsundToSchwerinDirect = {
+    from: "Stralsund Hauptbahnhof",
+    to: "Schwerin Hauptbahnhof",
+    via: [],
+    calStartDate: "2026-05-24",
+    calActive: 1,
+    calInactive: 4,
+    mapScreenshot: "map2.png",
+  };
+
+  const url = new URL("http://localhost:3000");
+  url.searchParams.set("from", STOPS.stralsund);
+  url.searchParams.set("to", STOPS.schwerin);
+  url.searchParams.set("calStart", "2026-05-24");
+
+  // *******************************************
+  // load data from url
+  // *******************************************
+  await page.goto(url.toString());
+  await expectPage(page, expStralsundToSchwerinViaRostock);
 
   // *******************************************
   // click on grey line in map -> change route
   // *******************************************
   await page.mouse.click(709, 369);
-  await timeout(1000); // again to be ready for screenshots
-
-  await expect(loc.cal.all).toHaveCount(5);
-  await expect(loc.cal.active).toHaveCount(1);
-  await expect(loc.cal.inactive).toHaveCount(4);
-  await expect(loc.cal.active).allToBeVisible();
-  await expect(loc.cal.inactive).allToBeHidden();
-  await expect(loc.cal.active.nth(0)).toHaveCSS("--color", C1);
-  await expect(loc.cal.activeStart.nth(0)).toContainText(NAMES.stralsund);
-  await expect(loc.cal.activeDest.nth(0)).toContainText(NAMES.schwerin);
-
-  await expect(loc.perl.connections).toHaveCount(1);
-  await expect(loc.perl.transfers).toHaveCount(0);
-  await expect(loc.perl.connections.nth(0)).toHaveCSS("--color", C1);
-  await expect(loc.perl.connections.nth(0)).toContainText(NAMES.stralsund);
-  await expect(loc.perl.connections.nth(0)).toContainText(NAMES.schwerin);
-
-  await expect(page).toHaveScreenshot("map2.png", {
-    stylePath: "tests_e2e/mapScreenshot.css",
-  });
+  await expectPage(page, expStralsundToSchwerinDirect);
 
   // *******************************************
   // click again to change back
   // *******************************************
   await page.mouse.click(568, 460);
-  await timeout(1000); // again to be ready for screenshots
-
-  await expect(loc.cal.all).toHaveCount(84);
-  await expect(loc.perl.connections).toHaveCount(2);
-  await expect(page).toHaveScreenshot("map1.png", {
-    stylePath: "tests_e2e/mapScreenshot.css",
-  });
+  await expectPage(page, expStralsundToSchwerinViaRostock);
 });
